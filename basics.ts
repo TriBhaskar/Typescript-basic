c2f44f5933b0ee75a91952541afcdb6f1fa8f710
TriBhaskar <panthribhaskar@gmail.com>
2024-04-01 21:22:12 +0530

    first commit

---
+++ basics.ts
@@ -0,0 +1,48 @@
+//primitives: number, string, boolean
+//more complex types: arrays, objects, functions
+//function types, parameters
+//object types, properties
+
+//primitives
+
+let age: number;
+age = 12;
+let userName: string;
+userName = "Bhaskar";
+let isInstructor: boolean;
+isInstructor = true;
+
+//more complex types
+let hobbies: string[];
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Bhaskar",
+  age: 30,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+//add 3 people to the people array
+people = [
+  { name: "Bhaskar", age: 30 },
+  { name: "Sachin", age: 40 },
+  { name: "Rahul", age: 50 },
+];
+
+//Type inference - typescript tries to understand what type a variable is based on the value you assign to it
+let course = "React - The Complete Guide";
+
+//course = 1234; //error
+
+//union types - more than one type is possible
+let courseName: string | number = "React - The Complete Guide";
+courseName = 1234;
